fe9d0636011c6a0097d29d243251e83c98188cdf
Radovan Marjanovic <marjanovic.radovan@gmail.com>
2020-07-01 22:00:18 +0200

    README file updated

--- app/models/armor.rb
+++ app/models/armor.rb
@@ -3,6 +3,6 @@ class Armor < ApplicationRecord
 
   belongs_to :robot
 
-  validates :armor_type, presence: true, inclusion: { in: %w(shield magnetic field invisible field) }
+  validates :armor_type, presence: true, inclusion: { in: %w(shield magnetic\ field invisible field) }
   validates :serial_number, uniqueness: true
 end
